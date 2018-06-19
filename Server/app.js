1b997e22a7c7f5f63e3c551e6599a9505661f8e5
saadkhan6670 <saad.khan6670@gmail.com>
2018-06-19 16:13:04 +0500

    server side changbes

--- Server/app.js
+++ Server/app.js
@@ -1,30 +1,25 @@
-var createError = require('http-errors');
+'use strict';
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var compression = require('compression');
+var bodyParser = require('body-parser');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+var cors = require('cors');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(compression());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//body parser middleware
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(express.static('public'));
+
+app.use('/api', require('./api'));
+
+
+app.listen(process.env.PORT,function () {
+  console.log('Running server on ' + process.env.PORT);
 });
 
 // error handler
