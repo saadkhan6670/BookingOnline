84020890eebc23fa7b4ab9dda51d882385b9ddbe
saadkhan6670 <saad.khan6670@gmail.com>
2018-07-14 01:06:23 +0500

    setups

--- Server/server.js
+++ Server/server.js
@@ -2,35 +2,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-const Sequelize = require('sequelize');
-var cors = require('cors');
 var port = process.env.PORT || 5000;
 
 var db = require('./models');
-
-// const sequelize = new Sequelize('bookingonline', 'root', "", {
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   operatorsAliases: false,
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// });
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
-// app.use(cors());
 
 //body parser middleware
 app.use(bodyParser.json()); // support json encoded bodies
@@ -41,6 +15,7 @@ app.use('/api', require('./api'));
 
 db.sequelize.sync().then(function(){
   app.listen(port, function () {
-  console.log('Running server on ' + port);
-});
+    console.log('Running server on ' + port);
+  });
 })
+ 
