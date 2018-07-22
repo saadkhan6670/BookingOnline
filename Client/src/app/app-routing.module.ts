71c94d0eca044486825807ece9ce36146373e31f
AtharZaib <syed.athar2k14@gmail.com>
2018-07-22 16:42:43 +0500

    Hotel list ui added

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent }      from './home/home.component';
+import { HotelsListComponent }      from './hotels-list/hotels-list.component';
+
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'hotel-list', component: HotelsListComponent }
 ];
 
 @NgModule({
